61ba55915f9986651fb9f80490d9fae8cd0940c8
Nemish Gorasiya <nemish.gorasiya@zuru.com>
2025-01-10 10:40:24 +0530

    feat: add horizontal scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { BackgroundCellAnimation } from './BackgroundRippleEffect'
+import HorizontalScroll from './horizontalScroll'
 import { TimelineDemo } from './TimelineDemo'
 
 function App() {
   return (
     <>
       <BackgroundCellAnimation />
+      <HorizontalScroll />
       <TimelineDemo />
     </>
   )
